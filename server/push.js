807f64907ceea0575af73c3e076f1d2b7c5ed3b5
Cristian Sanchez Flores <andygign40@gmail.com>
2022-10-27 23:15:09 -0500

    Borrar suscripciones que ya no son válidas

--- server/push.js
+++ server/push.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const urlsafeBase64 = require("urlsafe-base64");
 const vapid = require("./vapid.json");
 const webpush = require("web-push");
-const suscripciones = require("./subs-db.json");
+let suscripciones = require("./subs-db.json");
 
 webpush.setVapidDetails(
   "mailto:andygign40@gmail.com",
@@ -24,12 +24,36 @@ module.exports.addSubscription = (suscripcion) => {
 };
 
 module.exports.sendPush = (post) => {
+  
+  console.log('Mandando push');
 
-    suscripciones.forEach( (suscripcion, i) => {
+  const notificacionesEnviadas = [];
 
-        //webpush.sendNotification( suscripcion , post.titulo );
-        webpush.sendNotification(suscripcion, JSON.stringify(post));
+    suscripciones.forEach((suscripcion, i) => {
+      //webpush.sendNotification( suscripcion , post.titulo );
+      //webpush.sendNotification(suscripcion, JSON.stringify(post));
+      const pushProm = webpush
+        .sendNotification(suscripcion, JSON.stringify(post))
+        .then(console.log("Notificacion enviada "))
+        .catch((err) => {
+          console.log("Notificación falló");
 
+          if (err.statusCode === 410) {
+            // GONE, ya no existe
+            suscripciones[i].borrar = true;
+          }
+        });
+
+      notificacionesEnviadas.push(pushProm);
+    });
+
+    Promise.all(notificacionesEnviadas).then(() => {
+      suscripciones = suscripciones.filter((subs) => !subs.borrar);
+
+      fs.writeFileSync(
+        `${__dirname}/subs-db.json`,
+        JSON.stringify(suscripciones)
+      );
     });
 
 };
