257b6ff59d48a05bec5e1dd27034e061640a0375
Cristian Sanchez Flores <andygign40@gmail.com>
2022-10-27 23:05:25 -0500

    Configurar web-push

--- server/push.js
+++ server/push.js
@@ -2,7 +2,14 @@
 const fs = require("fs");
 const urlsafeBase64 = require("urlsafe-base64");
 const vapid = require("./vapid.json");
+const webpush = require("web-push");
 const suscripciones = require("./subs-db.json");
+
+webpush.setVapidDetails(
+  "mailto:andygign40@gmail.com",
+  vapid.publicKey,
+  vapid.privateKey
+);
 
 module.exports.getKey = () => {
   //return vapid.publicKey;
@@ -14,4 +21,14 @@ module.exports.addSubscription = (suscripcion) => {
     suscripciones.push(suscripcion);
 
     fs.writeFileSync(`${ __dirname }/subs-db.json`, JSON.stringify(suscripciones) );
+};
+
+module.exports.sendPush = (post) => {
+
+    suscripciones.forEach( (suscripcion, i) => {
+
+        webpush.sendNotification( suscripcion , post.titulo );
+
+    });
+
 };
